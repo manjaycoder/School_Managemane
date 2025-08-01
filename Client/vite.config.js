ae31eb14e663956be0b1ddda68852b8a5fa3645f
manjaycoder <manjay.verma.coder@gmail.com>
2025-08-01 11:31:00 +0530

    a

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,5 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-   
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
+
+
